529ad8f0d58a3255c57916d03d82809d4c25c408
Montgomery Kern <montgomerykern@icloud.com>
2022-04-10 23:52:00 -0400

    initial

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,16 @@
 <template>
-  <div>
-    <h1>Hello Nuxters! 👋</h1>
-    <p>
-      This page is rendered on the <strong>{{ rendering }}</strong>
-    </p>
-    <p v-if="rendering === 'server'">
-      First load or hard refresh is done on server side.
-    </p>
-    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
-    <ul>
-      <li>Refresh the page for server side rendering.</li>
-      <li>Click the links to see client side rendering.</li>
-    </ul>
-
-    <NuxtLink to="/about">About Page</NuxtLink>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Hello World</h1>
+        <p>This is a simple example of a Vue.js component.</p>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
 export default {
-  asyncData() {
-    return {
-      rendering: process.server ? 'server' : 'client'
-    }
-  }
+  name: 'IndexPage'
 }
 </script>
