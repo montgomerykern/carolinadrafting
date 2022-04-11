6ae4e13dc2cf9bdda6afdfafa061ae9eb3d74641
Montgomery Kern <montgomerykern@icloud.com>
2022-04-11 02:32:01 -0400

    new features

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,3 @@
 <template>
 <h1>About Us</h1>
 </template>
-
-<script>
-export default {
-  name: 'IndexPage'
-}
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    
+    <div>My blog navigation bar here</div>
+    <Nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+.container {
+  max-width: 700px;
+  margin: 0 auto;
+}
+</style>
