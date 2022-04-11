b0acc9210ebe0a841ed0b74b87e7245508ae3b37
Montgomery Kern <montgomerykern@icloud.com>
2022-04-11 00:16:03 -0400

    initial

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <h1>Hello World</h1>
-        <p>This is a simple example of a Vue.js component.</p>
-      </div>
-    </div>
-  </div>
+<h1>Hello World</h1>
 </template>
 
 <script>
